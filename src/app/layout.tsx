9536f1235121a6d4cd737f3cdbb4d38856306c47
Nishant Kumar <nishantkumar211106@gmail.com>
2026-04-08 21:40:50 +0530

    Updated npm version

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,6 +33,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Navbar />
         <MeshGradient />
         {children}
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 import Hero from "@/components/hero";
-import Navbar from "@/components/navbar";
 export default function Home() {
   return (
     <div className="relative min-h-screen">
-      <Navbar />
       <Hero />
     </div>
   );
